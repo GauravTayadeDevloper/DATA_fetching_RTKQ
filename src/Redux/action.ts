e108ce108c258c96a205e0513f43bc00bc11e358
GAURAV RAMAKANT TAYADE <110455312+GauravTayadeDevloper@users.noreply.github.com>
2024-09-04 22:34:45 +0530

    Update action.ts

--- src/Redux/action.ts
+++ src/Redux/action.ts
@@ -1,41 +1,5 @@
-
-export const FETCH_REPO_REQ ="FETCH_REPO_REQ"
-export const FETCH_REPO_SUCC ="FETCH_REPO_SUCC"
-export const FETCH_REPO_FAIL ="FETCH_REPO_FAIL"
-export const TOGGLE_FAVORITE = 'TOGGLE_FAVORITE';
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
-
-export const toggleFavorite = (repoId: number) => ({
-    type: TOGGLE_FAVORITE,
-    payload: repoId
-});
-
-export interface PublicRepo {
-    id: number;
-    name: string;
-    description: string;
-    html_url: string;
-    owner: {
-        login: string;
-        avatar_url: string;
-    };
-}
-
-interface FetchRepoReqAction {
-    type: typeof FETCH_REPO_REQ;
-}
-
-interface FetchRepoSuccAction {
-    type: typeof FETCH_REPO_SUCC;
-    payload: PublicRepo[]; 
-}
-
-interface FetchRepoFailAction {
-    type: typeof FETCH_REPO_FAIL;
-    payload: string; 
-}
-
-export type RepoActionTypes = FetchRepoReqAction | FetchRepoSuccAction | FetchRepoFailAction;
+import { PublicRepo } from './types';
 
 export const fetchRepos = createApi({
   reducerPath: 'fetchRepos',
